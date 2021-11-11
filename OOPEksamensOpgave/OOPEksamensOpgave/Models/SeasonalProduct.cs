bcc775995a0ace7382afab100e574b0a37f988bb
ah7877 <73359823+ah7877@users.noreply.github.com>
2021-11-12 00:55:59 +0100

    Add files via upload

---
+++ OOPEksamensOpgave/OOPEksamensOpgave/Models/SeasonalProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPEksamensOpgave.Models
+{
+    class SeasonalProduct : Product
+    {
+        private DateTime _seasonStartDate;
+        private DateTime _seasonEndDate;
+
+        /*
+         Noget der checker om den er i season
+         */
+
+        public DateTime SeasonStartDate
+        {
+            get { return _seasonStartDate; }
+            set { _seasonStartDate = value; }
+        }
+
+        public DateTime SeasonEndDate
+        {
+            get { return _seasonEndDate; }
+            set { _seasonEndDate = value; }
+        }
+    }
+}
---
+++ OOPEksamensOpgave/OOPEksamensOpgave/Models/Transaction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPEksamensOpgave.Models
+{
+    abstract class Transaction
+    {
+        private int _id;
+        private string _userName;
+        private DateTime _date;
+        private int _amount;
+
+
+        /*
+        implimentationer
+            ToString
+                ID, user, amount og Date
+        Excecute
+            udfører transaktionen
+        fornuftig constructer
+         */
+
+
+
+
+
+        public int ID
+        {
+            get { return _id; }
+            private set { _id = value; }
+        }
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public int Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+    }
+}
